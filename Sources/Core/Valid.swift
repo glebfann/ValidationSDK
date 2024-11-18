653d357b68ee6387711c809be834cd7a78066936
Gleb Fandeev <fandeev.business@gmail.com>
2024-11-18 19:14:36 +0300

    small refactoring

--- Sources/Core/Valid.swift
+++ Sources/Core/Valid.swift
@@ -6,3 +6,9 @@ public struct Valid<Tag, Value> {
     self.value = value
   }
 }
+
+extension Valid: CustomStringConvertible {
+  public var description: String {
+    String(describing: value)
+  }
+}
